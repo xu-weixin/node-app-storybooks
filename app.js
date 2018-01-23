4274e81963e1d9fb3f94b0228a44d73613136b65
xu-weixin <zealingstar@163.com>
2018-01-23 15:31:08 +0800

    set mongodb URI to a env variable

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send(process.env.mongoURI || 'Hello world'));
+app.get('/', (req, res) => res.send('Hello world'));
 
 app.listen(PORT, () => {
   console.log(`Server start on port ${PORT}`);
