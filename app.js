50f53bc7bd6225586e97676b2ae60f8c34a03892
xu-weixin <zealingstar@163.com>
2018-01-23 17:39:17 +0800

    add routes folder

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// load routers
+const auth_router = require('./routes/auth');
+app.use('/auth', auth_router);
+
 app.get('/', (req, res) => res.send('Hello world'));
 
 app.listen(PORT, () => {
